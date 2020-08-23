2272fbc0552cc9b2a7cbbf234901c915247d0388
wishmaster97 <t.abhijeet97@gmail.com>
2020-08-23 11:13:38 +0530

    prac 3 food app added

---
+++ React 240720/Week 4/Practice Project Food Web App/FoodDelivery-master/FoodDelivery-master/index.js
@@ -0,0 +1,17 @@
+console.log('Script Loaded!!')
+
+let mPara = document.getElementById('para');
+let mSpan = document.getElementById('span');
+let mImg = document.getElementById('img-food');
+
+mPara.onclick = function() {
+    alert('Para clicked!!')
+}
+
+mSpan.onclick = function() {
+    alert('Span clicked!!')
+}
+
+mImg.onclick = function() {
+    alert('Image clicked!!')
+}
