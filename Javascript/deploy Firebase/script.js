e7aec6e059eb8b3fa0287160c8f5d2645767314e
wishmaster97 <t.abhijeet97@gmail.com>
2020-12-25 10:52:46 +0530

    all done

--- Javascript/deploy Firebase/script.js
+++ Javascript/deploy Firebase/script.js
@@ -1,5 +1,5 @@
-var url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
-
+var url = 'https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+//var url = 'C:\Users\wish_\OneDrive\Desktop\React Tech\Javascript\deploy Firebase\content.json'
 console.log(url);
 
 
