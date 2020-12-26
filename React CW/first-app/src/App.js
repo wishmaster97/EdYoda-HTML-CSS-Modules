067fb234fe9b76a17ec12949b642746468a6fc38
wishmaster97 <t.abhijeet97@gmail.com>
2020-12-27 01:29:27 +0530

    react first project added

---
+++ React CW/first-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const fName ="Abhijeet";
+  //const checkVar = <span> Hi {fName} </span>
+  //console.log(checkVar);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Welcome <span> {fName}</span>
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
